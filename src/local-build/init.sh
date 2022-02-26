3ad5b386857f8edcec3e50554e6430600832128d
Arthur <arthurnunes.petee@gmail.com>
2022-02-26 17:39:32 -0300

    Remove old src folder

--- src/local-build/init.sh
+++
@@ -1,3 +0,0 @@
-# Download base images
-docker pull registry:2
-docker pull ros:melodic
--- src/scripts/start_novnc.sh
+++
@@ -1,6 +0,0 @@
-Xvfb $DISPLAY -screen $SCREEN $SCREEN_SIZE &
-sleep 5
-icewm &
-x11vnc -display $DISPLAY -nopw -listen localhost -xkb -ncache_cr -forever &
-ln -s /root/noVNC/vnc.html /root/noVNC/index.html
-/root/noVNC/utils/novnc_proxy #--vnc localhost:5900
